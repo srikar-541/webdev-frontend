d5a7f004afab8789ef098b0a8300cd96b76d5128
akulayash <akula.na@husky.neu.edu>
2020-04-06 18:20:52 -0400

    added editor profile

--- src/app/editor-profile/editor-profile.component.ts
+++ src/app/editor-profile/editor-profile.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class EditorProfileComponent implements OnInit {
 
   constructor() { }
+  pwd = ''
+  pwd2 = ''
+  phoneOld = '999-999-9999'
+  phoneNew = ''
+  articles = [ {title: 'title1'}, {title: 'title2'}, {title: 'title3'}];
+  updateProfile(){}
+  deleteArticle(){}
 
   ngOnInit(): void {
   }
