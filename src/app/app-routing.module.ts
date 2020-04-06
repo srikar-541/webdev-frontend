9f0c5f325e308c8f8cb6e0847d3191883c9f8290
akulayash <akula.na@husky.neu.edu>
2020-04-05 21:40:44 -0400

    hamburger

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {ArticleDetailComponent} from './article-detail/article-detail.component';
 import {NavigateBarComponent} from './navigate-bar/navigate-bar.component';
 import {SearchComponent} from './search/search.component';
+import {HamburgerComponentComponent} from './hamburger-component/hamburger-component.component';
 
 const routes: Routes = [
   {path: '', component: NavigateBarComponent},
   {path: 'search', pathMatch: 'full', component: SearchComponent},
   {path: 'search/:searchWord', pathMatch: 'full', component: SearchComponent},
-  {path: 'search/:searchWord/:index', component: ArticleDetailComponent}
+  {path: 'search/:searchWord/:index', component: ArticleDetailComponent},
+  {path: 'hamburger', component: HamburgerComponentComponent}
 ];
 
 @NgModule({
