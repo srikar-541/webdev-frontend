3960b45ba3b9fb82940668a5fdacb5d80c1a6f5d
akulayash <akula.na@husky.neu.edu>
2020-04-13 23:33:30 -0400

    nee amma3

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UsersServiceClient} from '../../services/users.service';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
@@ -10,15 +10,19 @@ export class AdminPageComponent implements OnInit {
 
   constructor(private service: UsersServiceClient) { }
   users: [];
+  role: '';
   ngOnInit(): void {
     this.service.getAllUsers().then(response => this.users = response );
   }
 
   updateUserRole(id) {
+    const gg = $('#role-fld').val();
+    console.log('update role' + gg + id);
   }
 
   deleteUser(id) {
-
+    console.log('delete' + id);
   }
+
 
 }
